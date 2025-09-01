2f56facbf63699bcc8e898d3774dae313c9f8f1f
noctiluc3nt <laura.mack@fu-berlin.de>
2025-09-01 21:43:53 +0200

    constants as functions not floats

--- constants.py
+++ constants.py
@@ -1,11 +1,139 @@
-cpcv=1.4
-Rd=287.05
-cp=1005.7
-rho=1.225
-Lv=2264.705*1000 #temperature independent version here, for ET use temperature dependent one
-karman=0.4
-g=9.81
-Runiversal=8.31451
-M_H2O=0.01802
-M_CO2=0.044
-M_CH4=0.01604
+#### Constants #####
+#' sigma
+#'
+#' Stefan-Boltzmann constant [W/m^2/K^4]
+#' @keywords internal
+def sigma():
+	return(5.67*10^(-8))
+
+
+#' cp
+#'
+#' heat capacity, constant pressure [J/(kg*K)]
+#' @keywords internal
+def cp():
+	return(1005.7)
+
+
+#' g
+#'
+#' gravitational accelaration [m/s^2]
+#' @keywords internal
+def g():
+	return(9.8062)
+
+
+#' clight
+#'
+#' speed of light [m/s]
+#' @keywords internal
+def clight():
+	return(299792458)
+
+
+#' csound
+#'
+#' speed of sound [m/s]
+#' @keywords internal
+def csound():
+	return(343)
+
+
+#' von Karman constant
+#'
+#' @keywords internal
+def karman():
+	return(0.4)
+
+
+#' R
+#'
+#' ideal gas constant [J/(mol*K)]
+#' @keywords internal
+def Runiversal():
+	return(8.31451)
+
+
+#' Rd
+#'
+#' gas constant for dry air [J/(kg*K)]
+#' @keywords internal
+def Rd():
+	return(287.05)
+
+
+#' Rv
+#'
+#' gas constant for water vapor [J/(kg*K)]
+#' @keywords internal
+def Rv():
+	return(461.52)
+
+
+#' gamma (ratio cp/cv)
+#'
+#' ratio of specific heat at constant pressure to that at constant volume (i.e. cp/cv = 1004 / 717 = 1.4) 
+#' @keywords internal
+def cpcv():
+	return(1.4)
+
+
+#' rhoAir
+#'
+#' density of air [kg/m^3]
+#' @keywords internal
+def rhoAir():
+	return(1.225) #at surface with p0 = 1013.25 hPa and T0 = 288 K 
+
+
+#' Lv
+#'
+#' latent heat of vaporization [J/kg]
+#' @param temp temperature [K] (optional)
+#' @keywords internal
+def Lv(temp=None):
+	if temp is None:
+		return(2264.705*1000)
+	else:
+		return((3147.5-2.37*temp)*1000) #Lv temperature dependence
+		
+
+
+#' M_H2O
+#'
+#' Molar mass of water
+#' @keywords internal
+def M_H2O():
+	return(0.01802)
+
+
+#' M_CO2
+#'
+#' Molar mass of carbon dioxide
+#' @keywords internal
+def M_CO2():
+	return(0.044)
+
+
+#' M_CH4
+#'
+#' Molar mass of methane
+#' @keywords internal
+def M_CH4():
+	return(0.01604)
+
+
+#' Charnock constant (alpha)
+#'
+#' Charnock constant (alpha) used to convert friction velocity to surface roughness length
+#' @keywords internal
+def alpha():
+	return(0.016)
+
+
+#' Earth's radius
+#'
+#' Earth's radius
+#' @keywords internal
+def R_earth():
+	return(6378137)
--- eddy_utils.py
+++ eddy_utils.py
@@ -23,10 +23,32 @@ def rotate_double(u,v,w):
     return u2, smaller_than_machine_epsilon(v2), smaller_than_machine_epsilon(w2)
 
 def ppt2rho(ppt,T_mean=288.15, pres = 101325, e = 0, gas="H2O"):
-    Vd=Runiversal*T_mean/(pres-e) #volume of dry air [m^3/mol]
+    Vd=Runiversal()*T_mean/(pres-e) #volume of dry air [m^3/mol]
     if gas == "H2O":
-        return ppt/1000*M_H2O/Vd
+        return ppt/1000*M_H2O()/Vd
     elif gas == "CO2":
-        return ppt/1000*M_CO2/Vd
+        return ppt/1000*M_CO2()/Vd
     elif gas == "CH4":
-        return ppt/1000*M_CH4/Vd
+        return ppt/1000*M_CH4()/Vd
+
+def Ts2T(Ts,q):
+    return Ts*(1+Rd()/Rv())*q
+
+def SNDcorrection(Ts_mean,u_mean,v_mean,cov_uw,cov_vw,cov_wTs,cov_qw,A=7/8,B=7/8):
+    if cov_qw is not None:
+        #cross-wind and SND correction
+        cov_wTs - 0.51*cov_qw + 2*Ts_mean/csound()**2*(A*u_mean*cov_uw + B*v_mean*cov_vw)
+    else:
+        #only cross-wind correction
+        return cov_wTs + 2*Ts_mean/csound()**2*(A*u_mean*cov_uw + B*v_mean*cov_vw)
+    
+def WPLcorrection(Ts_mean,q_mean,cov_wTs,rhow_mean,cov_wrhow,rhoc_mean=None,cov_wrhoc=None):
+    if rho_c is None: #water vapor flux
+        return((1+1.61*q_mean)*(cov_wrhow+rhow_mean/Ts_mean*cov_wTs)) #with M_L/M_w = 1.61
+    else: #other trace gas flux
+        return(cov_wrhoc+1.61*rhoc_mean/rhow_mean*cov_wrhow+(1+1.61*q_mean)*rhoc_mean/Ts_mean*cov_wTs)
+    
+
+def sos2Ts(sos):
+	return(sos^2/(cpcv()*Rd()))
+
